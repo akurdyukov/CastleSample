d2f2bc697ccec35ab5c48d64819174c098230e81
Alik Kurdyukov <alik@kurdyukov.com>
2022-10-08 19:32:16 +0400

    NLog instead of DynamicLogging to console

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Logging;
 
@@ -23,7 +26,12 @@ namespace CastleSample
                         .AddYamlFile($"appsettings.{env.EnvironmentName}.yml", optional: true);
                 })
                 .UseWindsorContainerServiceProvider()
-                .ConfigureLogging((context, builder) => builder.AddDynamicConsole())
+                .ConfigureLogging(b =>
+                {
+                    b.ClearProviders();
+                    b.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
